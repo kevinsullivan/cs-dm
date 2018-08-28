c4884bd7941065918132a5638326d4d38da74a99
Kevin Sullivan <sullivan@virginia.edu>
2018-08-28 09:30:23 -0400

    cleaning out S18 materials

--- lean/docstringerror.lean
+++
@@ -1,2 +0,0 @@
-theorem foo: ∀ P Q: Prop, P → Q → P ∧ Q :=
-  λ P Q hP hQ, and.intro hP hQ
--- lean/lecture2.lean
+++
@@ -1,7 +0,0 @@
-/-
-Proofs of negations.
-
-Negation is treated in very different ways in
-classical first-order predicate logic and in 
-the constructive logic of Lean. 
--/
