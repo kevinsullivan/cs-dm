b5b31463b7e64988e736cc3b369f6f2c19d44fb4
Kevin Sullivan <sullivan@virginia.edu>
2018-09-12 22:20:29 -0400

    Extended function examples.

--- 05_Functions/functions.lean
+++ 05_Functions/functions.lean
@@ -10,8 +10,8 @@ if given a proof of P returns a proof
 of Q.
 
 On the other hand, nothing in Lean will 
-prevent our using types such as bool and
-int as P and Q. In this case we'd say
+prevent our using types such as nat and
+bool as P and Q. In this case we'd say
 that P → Q, or nat → bool, is a type: a
 type of functions from P → Q, e.g., from
 nat to bool.
@@ -26,13 +26,77 @@ say, a lambda expression) of this type,
 ℕ → bool. 
 -/
 
-def positive (n: nat) : bool := 
+def zero_nat (n : ℕ) : nat := 0 -- explicit return type
+#check zero_nat
+#check zero_nat 5
+#reduce zero_nat 5
+
+def zero_nat': ℕ → ℕ := λ n : nat, (0 : nat)
+def zero_nat'': ℕ → ℕ := λ n, 0
+def zero_nat''' := λ n : nat, 0
+
+/- 
+EXERCISE: define a function, one_nat that takes any natural 
+number as an argument and always returns the natural number 
+1. Write it in traditional function definition notation and
+using lambda expressions and type inference. 
+-/
+
+def identity_nat (n : ℕ) : nat := n --explicit return type
+#check identity_nat
+#check identity_nat 5
+#reduce identity_nat 5
+
+/- 
+EXERCISE: define the same function, call it identity_nat'
+and write it using a lambda expression.  
+-/
+
+def double (n : ℕ) := 2 * n -- return type inferred
+#check double
+#check double 3
+#reduce double 3
+
+/-
+EXERCISE: Write it using a lambda expression.
+-/
+
+def square (n: ℕ) := n * n -- return type inferred
+#check square
+#check square 3
+#reduce square 3
+
+/-
+EXERCISE: Write it using a lambda expression.
+-/
+def isPositive (n: nat) : bool := 
     if n > 0 then tt else ff
 
-#check positive
+#check isPositive
+#check isPositive 0
+#check isPositive 1
+#check isPositive 2
+#reduce isPositive 0
+#reduce isPositive 1
+#reduce isPositive 2
 
-def uint32 (n: nat) : bool := 
-    if n >= 0 ∧ n < 2^32 then tt else ff
+/-
+EXERCISE: Write it using a lambda expression.
+-/
+
+/-
+Here's a function that returns boolean true
+(tt) if a given natural number n is greater
+than or equal to zero and less than 2^32. It
+is given using lambda notation. Write it with
+a prime on its name using ordinary function
+definition notation (as the functions above
+are written). 
+-/
+
+def uint32: ℕ → bool := 
+    λ n, 
+        if n >= 0 ∧ n < 2^32 then tt else ff
 
 #check uint32
 
@@ -47,7 +111,6 @@ def positive' : nat → bool :=
         (if n > 0 then tt else ff : bool)
 
 #check positive'
-
 #check λ n : nat, (if n > 0 then tt else ff : bool)
 #check λ n, (if n > 0 then tt else ff)
 #check λ n, n > 0
@@ -104,16 +167,6 @@ say, a lambda expression) of this type,
 ℕ → ℕ
 -/
 
-def double (n: ℕ) := 2 * n
-
-#check double
-#check double 3
-#reduce double 3
-
-def square (n: ℕ) := n * n
-
-#check square
-#reduce square 3
 
 /-
 We can also pass functions as arguments
@@ -124,11 +177,28 @@ def compose (f: ℕ → ℕ) (g: ℕ → ℕ) (x: ℕ) : ℕ :=
   f (g x)
 
 #check compose
+#check compose square double
 #reduce compose double double 3
 #reduce compose square double 3
 #reduce square (double 3)
 
 def do_twice (f : ℕ → ℕ) (x: ℕ) : ℕ := f (f x)
-
 #check do_twice
 #reduce do_twice square 3
+
+/-
+Let's write this as a lambda expression and 
+see what we get.
+-/
+
+def do_twice' : (ℕ → ℕ) → ℕ → ℕ := 
+    λ f x, f (f x)
+
+/-
+And that's a shorthand for this!
+-/
+
+def do_twice'' : (ℕ → ℕ) → ℕ → ℕ := 
+    λ f : (ℕ → ℕ),
+        λ (x : ℕ), 
+            f (f x)
